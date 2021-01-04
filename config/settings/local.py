3c98310a7801e6ccac3516ca2cfc94c49b1ded3b
Manas Ranjan Mallick <manas1104@gmail.com>
2021-01-05 02:41:34 +1000

    Updated allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
     default="r2hlowZmOMgHknK4lfDfXiHz15A09PRlEmV6pQf7IaOTbGkG5sHAMk3RI78qmLqQ",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "192.168.1.103", "manascodes.com", "192.168.1.102"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
 
 # CACHES
 # ------------------------------------------------------------------------------
