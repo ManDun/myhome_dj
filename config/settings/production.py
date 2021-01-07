24b958c449591aff93e9c9c554e7eb1c27ef4409
Manas Ranjan Mallick <manas1104@gmail.com>
2021-01-08 00:47:20 +1000

    Updated email

--- config/settings/production.py
+++ config/settings/production.py
@@ -134,6 +134,15 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://anymail.readthedocs.io/en/stable/esps
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 ANYMAIL = {}
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = env('EMAIL_HOST', default="smtp.gmail.com")
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = env('EMAIL_PORT', default=587)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Collectfast
 # ------------------------------------------------------------------------------
